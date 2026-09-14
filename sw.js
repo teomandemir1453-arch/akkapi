4e8872c6f8e13d094696fd4d6980556395d17531
teomandemir1453-arch <teomandemir1453@gmail.com>
2026-09-14 16:35:42 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const C='cs-v1';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./menu.js'])));self.skipWaiting();});
+self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    const cp=res.clone(); caches.open(C).then(c=>c.put(e.request,cp)); return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
