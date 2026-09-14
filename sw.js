0fab1009a408d0a0659402d8eb87439607da75d4
teomandemir1453-arch <teomandemir1453@gmail.com>
2026-09-14 20:24:57 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-const C='cs-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./menu.js'])));self.skipWaiting();});
-self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+const C='cs-v2';
+self.addEventListener('install',e=>{ self.skipWaiting(); });
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>clients.claim()));
+});
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-    const cp=res.clone(); caches.open(C).then(c=>c.put(e.request,cp)); return res;
-  }).catch(()=>caches.match('./index.html'))));
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      const cp=res.clone(); caches.open(C).then(c=>c.put(e.request,cp)); return res;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+  );
 });
